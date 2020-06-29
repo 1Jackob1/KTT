d740ff77d3bcec767171ad47aebc2edc05acb0c0
Jackob <saxolita@ya.ru>
2020-06-29 23:07:17 +0400

    KTT-5 Add core crud actions for user

---
+++ src/Util/MessageUtil.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Util;
+
+class MessageUtil
+{
+    public const SUCCESS = 'success';
+    public const ERROR = 'error';
+
+    public const CAN_NOT_SAVE = 'Can not save or update entity.';
+    public const VALIDATE_FORM = 'Error occurred while handling form.';
+    public const CAN_NOT_FIND_OBJECT = 'Can not find object.';
+}
